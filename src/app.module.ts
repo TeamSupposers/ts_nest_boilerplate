d86141cf26faf132f401e6cf0b8d259a674e600a
JUNGMINGYO <jywlsh3@naver.com>
2023-01-20 12:20:22 +0900

    nest js 세팅 - hot reload 설정 - env 설정(ConfigMoudel 전역모듈 설정) - ConfigService 설정(env 변수 사용)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
+  imports: [ 
+              ConfigModule.forRoot({ isGlobal: true }), // ConfigMoudel을 app.module 이외의 모듈에서 반복 import할 필요없는 전역 모듈로 설정
+              UsersModule,
+              UsersService,
+              UsersController
+            ], 
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService], // ConfigService를 활용해 환경변수 값 조회(DI)
 })
 export class AppModule {}
